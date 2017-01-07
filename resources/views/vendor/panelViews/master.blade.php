7efcd1d3f8423efe33406780f5e81b39160f26e5
Arka <arka.nitdgp14@gmail.com>
2017-01-08 02:27:21 +0530

    admincss removed

--- resources/views/vendor/panelViews/master.blade.php
+++ resources/views/vendor/panelViews/master.blade.php
@@ -30,8 +30,7 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="{{asset("css/adminmain.css")}}">
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
 
     <!-- jQuery Version 1.11.0 -->
     <script src="{{asset("packages/serverfireteam/panel/js/jquery-1.11.0.js")}}"></script>
